1cc55693482a98d7a84e0e7369ed5148713d837c
Aleksey Abu <direct.abu@yandex.ru>
2020-07-07 23:33:55 +0300

    8

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
-let popup = document.querySelector('.popup');
+//объявляем переменные для открытия и закрытия попапа
+let popup = document.querySelector('.popup_profile');
 let popupOpenButton = document.querySelector('.profile__title-buttom');
 let popupCloseButton = popup.querySelector('.popup__close-button');
+// popup card
+const popupCard = document.querySelector('.popup_card');
+const popupCardOpenButton = document.querySelector('.profile__button');
+const popupCardCloseButton = document.querySelector('.popup__close-button_card');
+
 
 let formElement = document.querySelector('.popup__form');
 
@@ -10,46 +16,12 @@ let name = document.querySelector('.profile__title');
 let job = document.querySelector('.profile__subtitle');
 
 const cardContainer = document.querySelector('.content__cards');
-
-
-function popupToggle() { //вызов попапа
-
-    if (!popup.classList.contains('popup_opened')) { 
-        nameInput.value = name.textContent;
-        jobInput.value = job.textContent;
-    } 
-
-    popup.classList.toggle('popup_opened'); //с помощью класса, открываем и закрываем попап
-
-}
-
-
-
-
-popupOpenButton.addEventListener('click', popupToggle );
-
-
-popupCloseButton.addEventListener('click', popupToggle);
-
-popupCloseButton.insertAdjacentHTML
-
-
-
 let saveClose = document.querySelector('.popup__save');
 
-function formSubmitHandler (evt) {//берем значения из профиля в попап
-    evt.preventDefault();
-    
-
-    name.textContent = nameInput.value
-    job.textContent = jobInput.value
-    
-    popupToggle();
-}
-
-
-
-formElement.addEventListener('submit', formSubmitHandler);
+const popupLink = document.querySelector('.popup-img__img');
+const popupName = document.querySelector('.popup-img__text');
+const popupImgClose = document.querySelector('.popup-img__close');
+const popupImgOpen = document.querySelector('.popup-img');
 
 const initialCards = [
     {
@@ -78,21 +50,127 @@ const initialCards = [
     }
 ];
 
+
+function popupToggle(popup) { //вызов попапа
+    popup.classList.toggle('popup_opened'); //с помощью класса, открываем и закрываем попап
+    console.log('privet')
+}
+
+
+popupCardOpenButton.addEventListener('click', function() {
+    popupToggle(popupCard)
+});
+popupCardCloseButton.addEventListener('click', function() {
+    popupToggle(popupCard)
+});
+
+
+//
+popupOpenButton.addEventListener('click', function(){
+
+    if (!popup.classList.contains('popup_opened')) { //проверяем, есть ли класс у попапа. Если нет, то переносим значения из профился
+        nameInput.value = name.textContent;
+        jobInput.value = job.textContent;
+    } 
+    popupToggle(popup)
+} );
+popupCloseButton.addEventListener('click', function(){
+    popupToggle(popup)
+    
+});
+
+popupCloseButton.insertAdjacentHTML
+
+function formSubmitHandler (evt) {//берем значения из профиля в попап
+    evt.preventDefault();
+    
+
+    name.textContent = nameInput.value
+    job.textContent = jobInput.value
+    
+    popupToggle(popup);
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
 function addCard(item){ //это функция добавления карточки с помощью темплайт тега
     const cardTemplate = document.querySelector('#card').content; //нашли шаблон карточки в template
     const cardElement = cardTemplate.cloneNode('true'); //клонируем шаблон карточки
     
+    cardElement.querySelector('.card__label-like').addEventListener('click', function(evt){
+        evt.target.classList.toggle('card__label-like_black');
+    });
+    
+    cardElement.querySelector('.card__basket').addEventListener('click', deleteCard);//обработчик здесь, так как майним карточки
+    cardElement.querySelector('.card__img').addEventListener('click', function(){
+        popupZoom(item);
+        console.log('privet')
+        popupToggle(popupImgOpen);
+    })
+    
+
     cardElement.querySelector('.card__title').textContent = item.name; //куда вставляем тайтл
     cardElement.querySelector('.card__img').src = item.link; //куда вставляем изображение
 
     console.log(item.name)
     console.log(item.link)
 
-    cardContainer.append(cardElement);
+    cardContainer.prepend(cardElement);
+
+    
 }
 
 initialCards.forEach (function(item) { //ф-ция обработки массива 
     addCard(item) //обрабатываем через эту функцию
 })
+
+document.querySelector('.popup-img__close').addEventListener('click',function(){
+    popupToggle(popupImgOpen);
+    console.log(popupImgOpen);
+})
+
+
+const formCardPopup = document.querySelector('.popup__save_card');
+const cardname = document.querySelector('.popup__input_cardname');
+const link = document.querySelector('.popup__input_link');
+const formElementCard = document.querySelector('.popup__form_card');
+
+console.log(formElementCard)
+
+function formSubmitHandlerCard(e) {
+    e.preventDefault();
+
+    const newname = cardname.value;
+    const newlink = link.value;
+    const newcard = { name: newname, link: newlink}
+
+    addCard(newcard)
+    popupToggle(popupCard);
+
+    cardname.value = '';
+    link.value = '';
+}
+
+formElementCard.addEventListener('submit', formSubmitHandlerCard);
+
+//f-on delete
+
+function deleteCard(e) {
+    const card = e.target.closest('.card');//получаем родительский элемент, который хотим удалить. Можно было бы curentTagte.parrentElement использовать
+
+    card.remove();
+}
+
+//функция открытия попапа с картинкой
+
+function popupZoom(item){
+    const linkPopup = item.link;
+    const namePopup = item.name;
+
+    popupLink.src = item.link;
+    popupName.textContent = item.name;
+}
+
 
 
